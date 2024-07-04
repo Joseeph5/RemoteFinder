934060c36cd8c5324c7463898ba06c10834856d4
Youssef Benamor <youssef.beenamor@gmail.com>
2024-07-04 13:56:32 +0100

    add some components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,18 @@
+import Background from './Background';
+import Header, { HeaderTop } from './Header';
+import Logo from './Logo';
+
 function App() {
-  return <>Hello!</>;
+  return (
+    <>
+      <Background />
+      <Header>
+        <HeaderTop>
+          <Logo />
+        </HeaderTop>
+      </Header>
+    </>
+  );
 }
 
 export default App;
