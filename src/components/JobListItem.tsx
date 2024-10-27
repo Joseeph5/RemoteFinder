a97e1ab265183a11094cb31a7cb007f4b213bf07
Youssef Benamor <youssef.beenamor@gmail.com>
2024-10-27 12:57:26 +0100

    update JobListItem

--- src/components/JobListItem.tsx
+++ src/components/JobListItem.tsx
@@ -1,7 +1,14 @@
 import BookmarkIcon from './BookmarkIcon';
+import { getDayDifference } from '../utils/job.ts';
 
 type JobListItemProps = {
-  jobItem: [];
+  jobItem: {
+    id: string;
+    company_logo: string;
+    title: string;
+    company_name: string;
+    publication_date: string;
+  };
   isActive: boolean;
 };
 
@@ -9,16 +16,23 @@ export default function JobListItem({ jobItem }: JobListItemProps) {
   return (
     <li>
       <a href={`#${jobItem?.id}`} className='job-item__link'>
-        <div className='job-item__badge'>{jobItem?.badgeLetters}</div>
+        <div className='job-item__badge'>
+          <img src={jobItem?.company_logo} alt='' width={50} height={50} />
+        </div>
 
         <div className='job-item__middle'>
           <h3 className='third-heading'>{jobItem?.title}</h3>
-          <p className='job-item__company'>{jobItem?.company}</p>
+          <p className='job-item__company'>{jobItem?.company_name}</p>
         </div>
 
         <div className='job-item__right'>
           <BookmarkIcon id={jobItem?.id} />
-          <time className='job-item__time'>{jobItem?.daysAgo}d</time>
+          <time className='job-item__time'>
+            {getDayDifference(
+              jobItem?.publication_date,
+              new Date().toDateString()
+            )}
+          </time>
         </div>
       </a>
     </li>
---
+++ src/utils/job.ts
@@ -0,0 +1,14 @@
+export function getDayDifference(date1: string, date2: string): string {
+  const timestamp1 = new Date(date1);
+  const timestamp2 = new Date(date2);
+
+  const differenceInTime = Math.abs(
+    timestamp2.getTime() - timestamp1.getTime()
+  );
+  const differenceInDays = Math.round(differenceInTime / (1000 * 3600 * 24));
+  console.log(differenceInDays.toString());
+
+  return differenceInDays === 1
+    ? `${differenceInDays.toString()} Day ago`
+    : `${differenceInDays.toString()} Days ago`;
+}
