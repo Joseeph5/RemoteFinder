fb0da09a0376e6774ce2b682ca78e6bd391fb3f1
Youssef Benamor <youssef.beenamor@gmail.com>
2024-11-12 14:28:29 +0100

    add BookmarksContext

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -1,6 +1,10 @@
 import { BookmarkFilledIcon } from '@radix-ui/react-icons';
+import { useContext } from 'react';
+import { BookmarksContext } from '../contexts/BookmarksContextProvider';
 
 export default function BookmarkIcon() {
+  const context = useContext(BookmarksContext);
+
   return (
     <button className='bookmark-btn'>
       <BookmarkFilledIcon className='filled' />
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import BookmarksContextProvider from './contexts/BookmarksContextProvider';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BookmarksContextProvider>
+        <App />
+      </BookmarksContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
